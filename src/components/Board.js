9ee09785db0e6e2ed589ccca28074218e31beff0
alexrivadeneira <alexrivs@gmail.com>
2018-04-16 20:54:31 -0700

    basic grid

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,7 @@ import Space from './Space';
 class Board extends Component {
   render() {
   	const boardStyle = {
-  		width: "600px",
+  		width: "520px",
   		border: "3px solid blue",
   		display: "block",
   		margin: "0 auto",
@@ -12,9 +12,17 @@ class Board extends Component {
 
   	let spaces = [];
 
-  	for(var i = 0; i < 100; i++){
-  		spaces.push(<Space key={i} />);
+  	for(var i = 0; i < this.props.playerBoard.length; i++){
+  		for(var j = 0; j < this.props.playerBoard[i].length; j++){
+  			spaces.push(
+  				<Space fill={this.props.playerBoard[i][j]}
+  				i={i}
+  				j={j}
+  				/>
+  			);
+  		}
   	}
+
 
     return (
     	<div style={boardStyle}>
--- src/components/Space.js
+++ src/components/Space.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 
 class Space extends Component {
   render() {
+
+    const colorKey = {
+      0: "blue",
+      1: "cyan",
+      2: "red",
+    };
+
     const spaceStyle = {
       width: "50px",
       height: "50px",
-      backgroundColor: "blue",
+      backgroundColor: colorKey[this.props.fill],
       display: "inline-block",
-      margin: "5px",
+      margin: "1px",
+      color: "#fff",
     }
+
     return (
       <div className="space" style={spaceStyle}>
+        {this.props.i}, {this.props.j}
       </div>
     );
   }
